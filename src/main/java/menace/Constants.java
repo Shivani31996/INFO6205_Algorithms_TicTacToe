3d4b1f956055daf045ee6b0ac3857ad9fe224324
Shivani31996 <shivani.chavan96@gmail.com>
2022-04-27 20:20:36 -0400

    Java Docs added

--- src/main/java/menace/Constants.java
+++ src/main/java/menace/Constants.java
@@ -1,5 +1,8 @@
 package menace;
 
+/**
+ * Below constants determine the proportion in which beads are added/removed to/from each matchbox
+ */
 public class Constants {
     final public static int ALPHA = 25;
     final public static int BETA = 3;
